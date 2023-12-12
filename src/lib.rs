ecd2fae858e20549f9de2626b7304db4f4d3ec63
Vinicius Port <caprilesport@gmail.com>
2023-12-12 15:20:57 -0300

    specify method and solvation for dft

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,18 @@ use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GedentConfig {
-    method: String,
+    dft_method: String,
     basis_set: String,
-    solvation_model: String,
+    dft_solvation_model: String,
     solvent: String,
 }
 
 impl ::std::default::Default for GedentConfig {
     fn default() -> Self {
         Self {
-            method: String::from("PBE0"),
+            dft_method: String::from("PBE0"),
             basis_set: String::from("def2-TZVP"),
-            solvation_model: String::from("CPCM"),
+            dft_solvation_model: String::from("CPCM"),
             solvent: String::from("water"),
         }
     }
